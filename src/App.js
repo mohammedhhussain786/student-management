1843828f4eebf65ddfca5aa2cfe90e0e7324b89b
mohammedhhussain786 <mohammedh.hussain786@gmail.com>
2026-07-31 23:27:53 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <h2 style={{textAlign:"center"}}>
+        Student Management App
+      </h2>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+function Navbar() {
+    return (
+        <nav style={{
+            background:"blue",
+            color:"white",
+            padding:"15px"
+        }}>
+            Student Management
+        </nav>
+    );
+}
+
+export default Navbar;
